d7836344f57b40480fec6a485c9d0a27412f2a6d
azapdev <a7mad3zap@gmail.com>
2025-12-15 14:04:48 +0200

    preloader

--- logic.js
+++ logic.js
@@ -4,7 +4,7 @@
 let preloader = document.querySelector(".preloader");
 setTimeout(function () {
   preloader.style.display = "none";
-}, 100);
+}, 3000);
 
 // ===============================
 // nav-list-toggle
