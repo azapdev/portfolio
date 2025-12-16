39259254ebf8c1b152ea3db01eb695f7bc9b6f14
azapdev <a7mad3zap@gmail.com>
2025-12-16 14:21:41 +0200

    final in toggal

--- logic.js
+++ logic.js
@@ -63,6 +63,9 @@ window.onload = () => {
 let togglebtn = document.querySelector("#toggle");
 let navlist = document.querySelector("#nav-list");
 let separetor = document.querySelector("#separetor");
+let frist = document.querySelector("#frist");
+let middel = document.querySelector("#middel");
+let last = document.querySelector("#last");
 let apper = true;
 if (window.innerWidth < 768) {
   navlist.style.display = "none";
@@ -73,14 +76,26 @@ togglebtn.addEventListener("click", () => {
   if (apper) {
     navlist.style.display = "block";
     separetor.style.display = "block";
+    document.body.style.overflow = "hidden";
     separetor.style.zIndex = "39";
     togglebtn.style.transform = "translateX(-75px)";
     togglebtn.style.transition = "transform 1s ease";
+    middel.style.display = "none";
+    frist.style.transform = "rotate(45deg) translate(1px, -1px)";
+    last.style.transform = "rotate(-45deg) translate(8px, -8px)";
+    frist.style.transition = "transform 0.5s ease";
+    last.style.transition = "transform 0.5s ease";
     apper = !apper;
   } else {
     navlist.style.display = "none";
     separetor.style.display = "none";
+    document.body.style.overflow = "auto";
     separetor.style.zIndex = "0";
+    middel.style.display = "block";
+    frist.style.transform = "rotate(0deg) translate(0px, 0px)";
+    last.style.transform = "rotate(0deg) translate(0px, 0px)";
+    frist.style.transition = "transform 0.5s ease";
+    last.style.transition = "transform 0.5s ease";
     apper = !apper;
     togglebtn.style.transform = "translateX(0px)";
   }
@@ -92,7 +107,14 @@ lis.forEach((li) => {
     if (window.innerWidth < 768) {
       navlist.style.display = "none";
       separetor.style.display = "none";
+      document.body.style.overflow = "auto";
+
       separetor.style.zIndex = "0";
+      middel.style.display = "block";
+      frist.style.transform = "rotate(0deg) translate(0px, 0px)";
+      last.style.transform = "rotate(0deg) translate(0px, 0px)";
+      frist.style.transition = "transform 0.5s ease";
+      last.style.transition = "transform 0.5s ease";
       apper = !apper;
       togglebtn.style.transform = "translateX(0px)";
     }
@@ -105,6 +127,13 @@ separetor.addEventListener("click", () => {
   navlist.style.display = "none";
   separetor.style.display = "none";
   separetor.style.zIndex = "0";
+  document.body.style.overflow = "auto";
+  middel.style.display = "block";
+  frist.style.transform = "rotate(0deg) translate(0px, 0px)";
+  last.style.transform = "rotate(0deg) translate(0px, 0px)";
+  frist.style.transition = "transform 0.5s ease";
+  last.style.transition = "transform 0.5s ease";
+
   apper = !apper;
   togglebtn.style.transform = "translateX(0px)";
 });
